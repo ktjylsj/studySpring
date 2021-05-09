b9fa9702d1b4fe2fe9037290daaf297457b45a81
taejink <tjkimts@naver.com>
2021-05-09 16:40:06 +0900

    20210509
    섹션3. 회원 관리 예제 - 백엔드 개발

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+// role에 맞도록 함수 이름짓는것이 특징
+// command shift t -> test 만들기
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {   // control + option + M -> extract to function
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * ID로 회원 조회
+     */
+    public Optional<Member> findOne(Long MemberId){
+        return memberRepository.findById(MemberId);
+    }
+}
